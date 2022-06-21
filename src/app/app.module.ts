f012f2e1b0e2bac5065e9e44decb72609ae83825
Larissa Nussbaumer <larissa_nussbaumer@gmx.ch>
2022-06-21 08:50:37 +0200

    Version 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
-
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
